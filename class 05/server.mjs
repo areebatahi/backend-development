95c8700b49e3cb145511f52389ee3b13017932d2
areebatahi <areebatahirarain@gmail.com>
2025-02-23 01:29:33 +0500

    Update CRUD

--- class 05/server.mjs
+++ class 05/server.mjs
@@ -2,11 +2,40 @@ import express from 'express'
 const app = express()
 const port = process.env.PORT || 3000
 
+let users = []
+
+app.use(express.json()) // data ko convert kry ga { json sy js }
+
+// getting data from server
 app.get('/', (req, res) => {
-    console.log('hi'); 
+  console.log('hi');
   res.send('Hello!')
 })
 
+app.get('/users', (req, res) => {
+  res.send(users)
+})
+
+// add data in server
+app.post('/useradd', (req, res) => {
+  console.log(req.body); // data ko server sy ly ga
+  users.push({ ...req.body, id: users.length + 1 }); // push in array 
+  res.send('user added successfully');
+})
+
+// delete data in server
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const index = users.findIndex(user => user.id === parseInt(id));
+  users.splice(index, 1)
+  res.send({ users });
+})
+
+// update data in server
+app.put('/users/:id',(req, res)=>{
+
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`) // show in terminal 
 })
